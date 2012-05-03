be9d9873667494c45fd50a814e3b22c931cc8208
xuongdat@gmail.com <xuongdat@gmail.com@3af2e002-0aba-93d0-baa3-9578a96cc5e8>
2012-05-03 15:10:45 +0000

    Sửa sql của customer review. version trước nếu user book 2 tours bất kì thì comment ở 1 tour sẽ bị duplicate theo số booking của users đó

--- tour.php
+++ tour.php
@@ -30,9 +30,12 @@
 		
 		$occurance_result = mysql_query('Select * From tour_occurance Where tourID='.$tourID);
 		
-		$result_rate = mysql_query('Select sum(rate) as sum, count(*) as count from tour, review, tour_booking where tour.tourID = review.tourID and tour_booking.tourID = tour.tourID and tour_booking.tourID="'.$tourID.'" having count >0');
+		$result_rate = mysql_query('Select sum(rate) as sum, count(*) as count from tour, review, tour_booking where tour.tourID = tour_booking.tourID and tour_booking.tour_booking_id = review.tour_booking_id and tour_booking.tourID="'.$tourID.'" having count >0');
 		
-		$result_review = mysql_query('Select * From tour_booking, review, tour Where tour_booking.tourID = tour.tourID and review.tourID = tour_booking.tourID and tour.tourID="'.$tourID.'" ORDER BY r_date DESC');
+		//$result_review = mysql_query('Select * From tour_booking, review, tour Where tour_booking.tourID = tour.tourID and review.tourID = tour_booking.tourID and tour.tourID="'.$tourID.'" ORDER BY r_date DESC');
+		
+		$result_review = mysql_query('Select * From tour_booking tb, review r, tour t Where tb.tourID = t.tourID And tb.tour_booking_id = r.tour_booking_id And t.tourID='.$tourID.' Order By r_date DESC');
+			
 	}
 	
 	include("tour.html");
